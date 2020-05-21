c93abce58f2f5baa003781eac24ec13c85314e69
Luis Marquezzini <marquezzini@dcc.ufrj.br>
2020-05-20 23:49:27 -0300

    Author username

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', msg => {
     if(command === '!hug'){
         var exampleEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
-        .setTitle('Abraços estão proibidos, ' + msg.author.username)
+        .setTitle('Abraços estão proibidos, ' + msg.author.username + "!")
         .setImage('https://i.imgur.com/X84T2Ot.gif')
         .setTimestamp()
         .setFooter('Em progresso by Kiyomin')
